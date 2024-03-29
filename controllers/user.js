eb4eb405359ac6be2a6074363751ddee0bde575f
Nuggu Phani Harsha <145762892+Nuggu-Phani-Harsha@users.noreply.github.com>
2024-03-29 16:08:14 +0530

    Add files via upload

---
+++ controllers/user.js
@@ -0,0 +1,44 @@
+const User=require("../models/user.js");
+
+module.exports.renderSignupForm = (req,res,)=>{
+    res.render("users/signup.ejs");
+};
+
+module.exports.renderLoginForm = (req,res,)=>{
+    res.render("users/login.ejs");
+};
+
+module.exports.login = async(req,res)=>{
+    req.flash("success","Welcome to the portal! You are Logged in!");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req,res)=>{
+    req.logout((err)=>{
+        if(err){
+            next(err);
+        }
+        req.flash("success","you are logged out!");
+        res.redirect("/login");
+    })
+};
+
+module.exports.signUp = async(req,res,)=>{
+    try{
+        let {username,email,password} =req.body;
+        const newUser = new User({email,username});
+        let registereduser= await User.register(newUser,password);
+        console.log(registereduser);
+        req.login(registereduser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success","User registered");
+            res.redirect('/listings');
+        }); 
+    } catch(e) {
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+};
